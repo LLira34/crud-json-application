ee9aecd21c8428982f89c8666967cda63f949617
Luis Lira <luislira1029@gmail.com>
2019-07-06 01:34:03 -0500

    Port

--- app/src/main/java/net/llira/crud_json/Config.java
+++ app/src/main/java/net/llira/crud_json/Config.java
@@ -2,15 +2,15 @@ package net.llira.crud_json;
 
 public class Config {
     public static final String URL_ADD =
-            "http://192.168.0.25/Android/CRUD/addEmp.php";
+            "http://localhost/Android/CRUD/addEmp.php";
     public static final String URL_GET_ALL =
-            "http://192.168.0.25/Android/CRUD/getAllEmp.php";
+            "http://localhost/Android/CRUD/getAllEmp.php";
     public static final String URL_GET_EMP =
-            "http://192.168.0.25/Android/CRUD/getEmp.php?id=";
+            "http://localhost/Android/CRUD/getEmp.php?id=";
     public static final String URL_UPDATE_EMP =
-            "http://192.168.0.25/Android/CRUD/updateEmp.php";
+            "http://localhost/Android/CRUD/updateEmp.php";
     public static final String URL_DELETE_EMP =
-            "http://192.168.0.25/Android/CRUD/deleteEmp.php?id=";
+            "http://localhost/Android/CRUD/deleteEmp.php?id=";
 
     public static final String KEY_EMP_ID = "id";
     public static final String KEY_EMP_NAME = "name";
